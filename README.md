f7afcc3d11411fe683dc23ac819bf46cb51b1d1c
Kris <kristhecanadian101@gmail.com>
2020-03-15 18:34:39 -0400

    password recovery feature
    password recovery via email using token has been added

--- README.md
+++ README.md
@@ -4,3 +4,22 @@ Inspired by [@CoreyMSchafer](https://github.com/CoreyMSchafer) 's Flask Tutorial
 
 The goal of this project was to learn how to use create a Web Application using the Flask Framework.
 
+Dependencies:
+- Python 3.7+
+- Pillow
+- flask_login
+- flask-bcrypt
+- flask-sqlalchemy
+- flask-wtf
+- flask-mail
+- flask
+
+
+> Make sure to create app/secrets.py and in your secrets.py add the following:
+> 
+>SECRET_KEY = 'SOME RANDOM STRING'
+>MAIL_USER = 'USERNAME'
+>MAIL_PASS = 'PASSWORD'
+>
+>Note: If you are using Gmail. You need to turn on '
+Less secure app access' in your Gmail Account Security.
--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from app.secrets import SECRET_KEY
+from app.secrets import SECRET_KEY, MAIL_USER, MAIL_PASS
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = MAIL_USER
+app.config['MAIL_PASSWORD'] = MAIL_PASS
+mail = Mail(app)
 
 from app import routes
